27844564599cbca2d99144fb54e32a37785390dc
dev <dev@qiyutech.tech>
2021-07-15 16:43:43 +0800

    update type

--- rule/baidu_hot/view.ts
+++ rule/baidu_hot/view.ts
@@ -1,8 +1,9 @@
 import {DataType} from "./dt";
+import {MonitorRuleViewBlock} from "../../src/shared/dt";
 
 /**
  * 对比函数
  */
-let fn = async (v1: DataType | null, v2: DataType) => {
-
+let fn = async (v1: DataType | null, v2: DataType): Promise<MonitorRuleViewBlock[]> => {
+    return []
 }
